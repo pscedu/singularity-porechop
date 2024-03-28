33744b0b80cf5d9c6073b2c517d96f9ced267aa0
icaoberg <icaoberg@andrew.cmu.edu>
2024-03-27 22:22:32 -0400

    Fixed test for GitHub

--- 0.2.4/test.sh
+++ 0.2.4/test.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-wget -nc https://github.com/rieseberglab/fastq-examples/raw/master/data/HI.4019.002.index_7.ANN0831_R1.fastq.gz
+wget -nc -O ./HI.4019.002.index_7.ANN0831_R1.fastq.gz https://github.com/rieseberglab/fastq-examples/raw/master/data/HI.4019.002.index_7.ANN0831_R1.fastq.gz
 
 ./singularity-porechop-0.2.4.sif --input HI.4019.002.index_7.ANN0831_R1.fastq.gz \
   --output filtered.fastq.gz \
