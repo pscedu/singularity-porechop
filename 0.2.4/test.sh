422fe018aa57b28d5031843dba3e4b00a0acbc44
icaoberg <icaoberg@andrew.cmu.edu>
2024-03-27 22:27:06 -0400

    Fixed test for GitHub

--- 0.2.4/test.sh
+++ 0.2.4/test.sh
@@ -4,7 +4,9 @@ set -x
 
 wget -nc -O ./HI.4019.002.index_7.ANN0831_R1.fastq.gz https://github.com/rieseberglab/fastq-examples/raw/master/data/HI.4019.002.index_7.ANN0831_R1.fastq.gz
 
-./singularity-porechop-0.2.4.sif --input HI.4019.002.index_7.ANN0831_R1.fastq.gz \
-  --output filtered.fastq.gz \
-  --threads 1 \
-  --discard_middle
+if [ -f HI.4019.002.index_7.ANN0831_R1.fastq.gz ]l; then
+   ./singularity-porechop-0.2.4.sif --input HI.4019.002.index_7.ANN0831_R1.fastq.gz \
+     --output filtered.fastq.gz \
+     --threads 1 \
+     --discard_middle
+fi
